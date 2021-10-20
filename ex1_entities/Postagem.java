8c8ebf2eabfdb91a360a8d61503fae3c16a82b7b
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-10-20 09:28:14 -0300

    class Postagem ready

--- ex1_entities/Postagem.java
+++ ex1_entities/Postagem.java
@@ -1,5 +1,22 @@
 package ex1_entities;
 
-public class Postagem {
+import java.time.LocalDateTime;
 
+public class Postagem {
+	
+	private final String conteudo;
+	private final LocalDateTime dataHora;
+	
+	public Postagem(String conteudo, LocalDateTime dataHora) {
+		this.conteudo = conteudo;
+		this.dataHora = dataHora;
+	}
+
+	public String getConteudo() {
+		return conteudo;
+	}
+
+	public LocalDateTime getDataHora() {
+		return dataHora;
+	}
 }
