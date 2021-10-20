892f8af3bc52fa53931884728c4b2cb8fcb65210
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-10-20 09:24:34 -0300

    Adding all files, first commit

---
+++ ex1_application/Program.java
@@ -0,0 +1,11 @@
+package ex1_application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		//Author: Joao Vitor Souza Pioner 20/10/21
+		
+		
+	}
+
+}
---
+++ ex1_entities/Postagem.java
@@ -0,0 +1,5 @@
+package ex1_entities;
+
+public class Postagem {
+
+}
