da4d9ab4898e0cf02c8582a8d30aa50b9ff26a80
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-10-26 09:17:42 -0300

    First commit ex3

---
+++ ex3_application/Program.java
@@ -0,0 +1,9 @@
+package ex3_application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		//Author: Joao Vitor Souza Pioner 26/10/21
+	}
+
+}
---
+++ ex3_entities/Serie.java
@@ -0,0 +1,5 @@
+package ex3_entities;
+
+public class Serie {
+
+}
