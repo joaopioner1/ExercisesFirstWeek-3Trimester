caa3a61dd6291fc0aef7b34dcee4234a9d18cac7
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-10-26 10:05:38 -0300

    Finished class Temporada

--- ex3_entities/Temporada.java
+++ ex3_entities/Temporada.java
@@ -4,10 +4,10 @@ import java.time.Duration;
 import java.util.ArrayList;
 
 public class Temporada {
-	
+
 	private final String titulo;
 	private final ArrayList<Episodio> list_eps;
-	
+
 	public Temporada(String titulo) {
 		this.titulo = titulo;
 		this.list_eps = new ArrayList<>();
@@ -16,10 +16,40 @@ public class Temporada {
 	public String getTitulo() {
 		return titulo;
 	}
-	
+
 	public void addEpisodio(String titulo, Duration d) {
 		list_eps.add(new Episodio(titulo, d));
 	}
+
+	public Episodio getEpisodio(int pos) {
+		return list_eps.get(pos);
+	}
+
+	public void assistirProximo() {
+		for (int i = 0; i < list_eps.size(); i++) {
+			if (list_eps.get(i).isAssistido() == false) {
+				list_eps.get(i).assistir();
+				break;
+			}
+		}
+	}
+
+	public boolean isAssistido() {
+		int count = 0;
+		for (Episodio e : list_eps) {
+			if (e.isAssistido() == true) {
+				count++;
+			} 
+		}
+		if (count == list_eps.size()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 	
-	
+	public int getTamanho() {
+		return list_eps.size();
+	}
 }
